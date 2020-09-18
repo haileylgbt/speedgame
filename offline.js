60126f909aaf54208000326b8265a7d926cc85e1
Hailey <pacboy2004@gmail.com>
2020-09-18 17:15:27 +1000

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,16 @@
+﻿{
+	"version": 1600413135,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite3-sheet0.png",
+		"images/tilemap.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/tilemap2.png"
+	]
+}
