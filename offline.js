0b0f9ecb1ab975fbc6a2b3a07aa559c848904619
Hailey <pacboy2004@gmail.com>
2020-09-20 12:17:26 +1000

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1600413135,
+	"version": 1600568139,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -11,6 +11,14 @@
 		"images/sprite2-sheet0.png",
 		"images/sprite4-sheet0.png",
 		"images/sprite5-sheet0.png",
-		"images/tilemap2.png"
+		"images/tilemap2.png",
+		"media/sg_die.m4a",
+		"media/sg_die.ogg",
+		"media/sg_jump.m4a",
+		"media/sg_jump.ogg",
+		"media/sg_thud.m4a",
+		"media/sg_thud.ogg",
+		"media/sg_walljump.m4a",
+		"media/sg_walljump.ogg"
 	]
 }
